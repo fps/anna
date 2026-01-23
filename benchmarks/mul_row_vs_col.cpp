8362fc30e9c94ecd69bb39a3f9c89fb11d6c0a0c
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-01-23 12:38:03 +0100

    measure something

--- benchmarks/mul_row_vs_col.cpp
+++ benchmarks/mul_row_vs_col.cpp
@@ -6,12 +6,13 @@ template<int O1, int O2>
 static void mul(benchmark::State & state)
 {
   Eigen::Matrix<float, 16, 16, O1> m;
-  Eigen::Matrix<float, 16, 1024, O2> n;
+  Eigen::Matrix<float, 16, 64, O2> n;
+  Eigen::Matrix<float, 16, 64> o;
 
   for (auto _ : state)
   {
-    auto r = m * n;
-    escape(&r);
+    o = m * n;
+    escape(&o);
   }
 }
 
