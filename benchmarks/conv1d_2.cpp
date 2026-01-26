6177bbbca3b630e319caed598eac167cdc38c4ba
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-01-26 09:53:31 +0100

    measure some more things

--- benchmarks/conv1d_2.cpp
+++ benchmarks/conv1d_2.cpp
@@ -3,7 +3,7 @@
 #include <anna/conv1d_double_buffer.hpp>
 #include <anna/benchmark.hpp>
 
-template<int KernelSize, int Dilation>
+template<int KernelSize, int Dilation = 1, bool DoZero = false>
 struct conv1d
 {
   template<typename T1, typename T2, typename T3>
@@ -12,40 +12,68 @@ struct conv1d
   }
 };
 
-template<int Dilation>
-struct conv1d<3, Dilation>
+template<int Dilation, bool DoZero>
+struct conv1d<3, Dilation, DoZero>
 {
   template<typename T1, typename T2, typename T3>
   static void inline process(T1 const & weights, Eigen::MatrixBase<T2> const & input, Eigen::MatrixBase<T3> const & const_output)
   {
     Eigen::MatrixBase<T3> & output = const_cast<Eigen::MatrixBase<T3> &>(const_output);
-    output += 
-        (std::get<0>(weights) * input)
-      + (std::get<1>(weights) * input)
-      + (std::get<2>(weights) * input);
+
+    if constexpr (DoZero)
+    {
+      output.noalias() = 
+          (std::get<0>(weights) * input)
+        + (std::get<1>(weights) * input)
+        + (std::get<2>(weights) * input);
+    }
+    else
+    {
+      output.noalias() += 
+          (std::get<0>(weights) * input)
+        + (std::get<1>(weights) * input)
+        + (std::get<2>(weights) * input);
+    }
+  }
+
+  template<typename T1, typename T2, typename T3, typename T4>
+  static void inline process_with_bias(T1 const & weights, Eigen::MatrixBase<T2> const & bias, Eigen::MatrixBase<T3> const & input, Eigen::MatrixBase<T4> const & const_output)
+  {
+    process(weights, input, const_output);
+    Eigen::MatrixBase<T3> & output = const_cast<Eigen::MatrixBase<T3> &>(const_output);
+    output.template colwise() += bias;
   }
 };
 
-template<int N>
+template<int N, int Dilation, bool DoAdd, bool DoBias>
 static void run(benchmark::State & state)
 {
   const std::array<Eigen::Matrix<float, 16, 16>, 3> weights = anna::make_weights<float, 3, 16, 16>(1.0);
   const Eigen::Matrix<float, 16, N> input = Eigen::Matrix<float, 16, N>::Ones();
+  const Eigen::Vector<float, 16> bias = Eigen::Vector<float, 16>::Ones();
   Eigen::Matrix<float, 16, N> output;
 
   for (auto _ : state)
   {
-    // process_conv1d<3, Eigen::Matrix<float, 16, 16*3>, Eigen::Matrix<float, 16, 4096>, Eigen::Matrix<float, 16, 4096>(weights, input, output);
-    // process_conv1d(weights, input, integer<3>(), integer<1024>());
-    conv1d<3, 1024>::template process(weights, input, output);
+    if constexpr (DoBias)
+    {
+      conv1d<3, 1024, DoAdd>::template process_with_bias(weights, bias, input, output);
+    }
+    else
+    {
+      conv1d<3, 1024, DoAdd>::template process(weights, input, output);
+    }
     escape(&output);
   }
 }
 
-BENCHMARK(run<64>);
-BENCHMARK(run<128>);
-BENCHMARK(run<256>);
-BENCHMARK(run<512>);
+BENCHMARK(run<64, 1, false, false>);
+
+BENCHMARK(run<64, 1, false, true>);
+
+BENCHMARK(run<64, 1, true, false>);
+
+BENCHMARK(run<64, 1, true, true>);
 
 BENCHMARK_MAIN();
 
