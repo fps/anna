5cf87df45d4afc33f976ea038158b063af374abe
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-01-27 09:56:10 +0100

    trying adding/non-adding specialization via type

--- benchmarks/conv1d_2.cpp
+++ benchmarks/conv1d_2.cpp
@@ -3,7 +3,25 @@
 #include <anna/benchmark.hpp>
 #include <anna/conv1d_make_weights.hpp>
 
-template<int KernelSize, int Dilation>
+template<bool Add>
+struct assign
+{
+  template<typename T1, typename T2>
+  static inline void process(Eigen::MatrixBase<T1> const & arg, Eigen::MatrixBase<T2> const & const_result)
+  {
+    Eigen::MatrixBase<T2> & result = const_cast<Eigen::MatrixBase<T2> &>(const_result);
+    if constexpr (Add)
+    {
+      result.noalias() += arg;
+    }
+    else
+    {
+      result.noalias() = arg;
+    }
+  }
+};
+
+template<int KernelSize, bool UseSpecialization>
 struct conv1d
 {
   template<typename T1, typename T2, typename T3>
@@ -12,19 +30,28 @@ struct conv1d
   }
 };
 
-template<int Dilation>
-struct conv1d<3, Dilation>
+template<int Dilation, typename Assign>
+struct conv1d<3, Dilation, Assign>
 {
   template<typename T1, typename T2, typename T3>
   static void inline process(T1 const & weights, Eigen::MatrixBase<T2> const & input, Eigen::MatrixBase<T3> const & const_output, const int n, const int input_head, const int output_head)
   {
     Eigen::MatrixBase<T3> & output = const_cast<Eigen::MatrixBase<T3> &>(const_output);
+    /* 
 
     // output.middleCols(output_head - n, n).array() = 0;
     output.middleCols(output_head - n, n).noalias() += 
         (std::get<0>(weights) * input.template middleCols(input_head - n, n))
       + (std::get<1>(weights) * input.template middleCols(input_head - (Dilation + n), n))
       + (std::get<2>(weights) * input.template middleCols(input_head - ((Dilation * 2) + n), n));
+    */
+    Assign::process(
+        (std::get<0>(weights) * input.template middleCols(input_head - n, n))
+      + (std::get<1>(weights) * input.template middleCols(input_head - (Dilation + n), n))
+      + (std::get<2>(weights) * input.template middleCols(input_head - ((Dilation * 2) + n), n)),
+      output.middleCols(output_head - n, n)
+    );
+
   }
 
   template<typename T1, typename T2, typename T3, typename T4>
@@ -48,7 +75,7 @@ static void run_with_bias(benchmark::State & state)
   {
     for (int n = 0; n < 750; ++n)
     {
-      conv1d<3, Dilation>::template process_with_bias(weights, bias, input, output, N, 4096, 4096);
+      conv1d<3, Dilation, assign<true>>::template process_with_bias(weights, bias, input, output, N, 4096, 4096);
     }
   }
 }
@@ -64,7 +91,7 @@ static void run(benchmark::State & state)
   {
     for (int n = 0; n < 750; ++n)
     {
-      conv1d<3, Dilation>::template process(weights, input, output, N, 4096, 4096);
+      conv1d<3, Dilation, assign<false>>::template process(weights, input, output, N, 4096, 4096);
     }
   }
 }
