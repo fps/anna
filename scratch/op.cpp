6495c7a2275b60ad46f84db655f3240db086f41a
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-02-09 13:55:23 +0100

    hmmm

--- scratch/op.cpp
+++ scratch/op.cpp
@@ -3,17 +3,53 @@
 #include <Eigen/Core>
 
 template<typename T, int OutputChannels, int InputChannels, int MaxBlockSize, typename NextOpType>
-struct linear_op
+struct linear
 {
   Eigen::Matrix<T, InputChannels, MaxBlockSize> m_input;
   Eigen::Matrix<T, OutputChannels, InputChannels> m_matrix;
-  static int m_input_head = MaxBlockSize;
+  static const int m_input_head = MaxBlockSize;
 
-  NextOpType & m_next_op;
+  NextOpType m_next_op;
+
+  linear() :
+    m_matrix(Eigen::Matrix<T, OutputChannels, InputChannels>::Zero())
+  {
+    for (int index = 0; index < std::min(InputChannels, OutputChannels); ++index)
+    {
+      m_matrix(index, index) = 1;
+    }
+  }
+
+  auto & end() { return m_next_op.end(); }
+
+  auto & input() { return m_input; }
+
+  int input_head() { return m_input_head; }
 
   inline void process(const int n)
   {
-    m_next_op.m_input.middleCols(m_next_op.m_input_head, n) = m_matrix * m_input.middleCols(m_input_head, n);
+    m_next_op.input().middleCols(m_next_op.input_head() - n, n).noalias() = m_matrix * m_input.middleCols(m_input_head - n, n);
+
+    m_next_op.process(n);
+  }
+};
+
+template<typename T, int Channels, typename NextOpType>
+struct vector_add
+{
+  Eigen::Vector<T, Channels> m_value;
+
+  NextOpType m_next_op;
+
+  auto & end() { return m_next_op.end(); }
+
+  auto & input() { return m_next_op.input(); }
+
+  int input_head() { return m_next_op.input_head(); }
+
+  inline void process(const int n)
+  {
+    m_next_op.input().middleCols(m_next_op.input_head() - n, n).colwise().noalias() += m_value;
     m_next_op.process(n);
   }
 };
@@ -24,21 +60,70 @@ struct output
   Eigen::Matrix<T, InputChannels, MaxBlockSize> m_input;
   static const int m_input_head = MaxBlockSize;
 
+  auto & end() { return *this; }
+
+  auto & input() { return m_input; }
+
+  int input_head() { return m_input_head; }
+
   inline void process(const int n)
   {
-    
+    // NO OP 
   }
 };
 
+template<typename T, int Nominator, int Denominator, typename NextOpType>
+struct scalar_multiple
+{
+  NextOpType m_next_op;
+
+  static constexpr T m_value = (T)Nominator/(T)Denominator;
+
+  auto & end() { return m_next_op.end(); }
+
+  auto & input() { return m_next_op.input(); }
+
+  int input_head() { return m_next_op.input_head(); }
+
+  inline void process(const int n)
+  {
+    m_next_op.input().middleCols(m_next_op.input_head() - n, n).array() *= m_value;
+    m_next_op.process(n);
+  }
+};
+
+template<int Channels>
 static inline void run(benchmark::State & state)
 {
-  linear_op<float, 4, 8, 64, 
-    linear_op<float, 8, 4, 64, 
-      output<float, 8, 64>
-    >
-  > net;
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  scalar_multiple<float, 17, 3, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  linear<float, Channels, Channels, 64, 
+  output<float, Channels, 64
+  >>>>>>>>>>>> net;
+
+  net.m_input = Eigen::Matrix<float, Channels, 64>::Ones();
+
+  for (auto _ : state)
+  {
+    for (int index = 0; index < 750; ++index)
+    {
+      net.process(64);
+    }
+  }
+
+  // std::cout << net.end().m_input;
 }
 
-BENCHMARK(run);
+BENCHMARK(run<8>);
+BENCHMARK(run<16>);
+BENCHMARK(run<32>);
 
 BENCHMARK_MAIN();
