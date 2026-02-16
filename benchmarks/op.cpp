3ad35aa7a1d3ac11d0322bc579b5dd253679d91d
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-02-16 13:28:03 +0100

    move some benchmarks out

--- benchmarks/op.cpp
+++ benchmarks/op.cpp
@@ -7,58 +7,6 @@
 using namespace anna::op;
 
 template<int Channels>
-static inline void run_linear10(benchmark::State & state)
-{
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  output<float, Channels, 64
-  >>>>>>>>>>> net;
-
-  net.m_input = Eigen::Matrix<float, Channels, 64>::Ones();
-
-  net.template set<0>(Eigen::Matrix<float, Channels, Channels>::Zero());
-
-  for (auto _ : state)
-  {
-    for (int index = 0; index < 750; ++index)
-    {
-      net.process(64);
-    }
-  }
-
-  // std::cout << net.end().m_input;
-}
-
-template<int Channels>
-static inline void run_linear3(benchmark::State & state)
-{
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  linear1<float, Channels, 64, 
-  output<float, Channels, 64
-  >>>> net;
-
-  Eigen::Matrix<float, Channels, 64> input = Eigen::Matrix<float, Channels, 64>::Zero();
-
-  for (auto _ : state)
-  {
-    for (int index = 0; index < 750; ++index)
-    {
-      process(net, input, 64);
-    }
-  }
-}
-   
-
-template<int Channels>
 static inline void run_conv1d_bias(benchmark::State & state)
 {
   conv1d<float, Channels, Channels, 3, 1024, 64, 
@@ -218,7 +166,5 @@ BENCHMARK(run_conv1d_bias<(channels)16>);
 BENCHMARK(run_conv1d_bias_tanh20<(channels)16>);
 BENCHMARK(run_conv1d_bias20<(channels)16>);
 BENCHMARK(run_conv1d_bias20<(channels)32>);
-BENCHMARK(run_linear3<(channels)16>);
-BENCHMARK(run_linear10<(channels)16>);
 
 BENCHMARK_MAIN();
