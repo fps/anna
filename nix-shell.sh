c47fa8d126943078fd2b37623a6108cfec362b4e
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-04-13 14:33:46 +0200

    remove non-necessary stuff

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,1 +1,1 @@
-nix-shell -p eigen nlohmann_json pkg-config libsndfile gbenchmark gtest blitz boost meson ninja inotify-tools
+nix-shell -p eigen nlohmann_json pkg-config libsndfile gbenchmark gtest boost meson ninja 
